ec0ebc95f51c996c387306a7afaff80edad38d85
Arris Ray <arris.ray@motionreality.com>
2012-10-17 17:52:20 -0400

    Implement basic solution.

--- problems/counting_nucleotides/main.cpp
+++ problems/counting_nucleotides/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
 #include <string>
+#include <problems/counting_nucleotides/problem.h>
+
+using namespace mancobian::rosalind::problems;
 
 int main(int argc, char **argv)
 {
+    std::string output;
+    counting_nucleotides::Problem problem;
+    problem.solve("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC", output);
+
+    std::cout << output << std::endl;
+    system("pause");
     return 0;
 }
